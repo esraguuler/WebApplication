dbd315b6747a85da20ee566900fe30b446b59a0c
esraguuler <65496194+esraguuler@users.noreply.github.com>
2023-09-15 00:43:16 +0300

    Web

---
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Data.SqlClient;
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+
+            [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = "data source=LAPTOP-1KQ0BKEU; database=ymgk; integrated security= SSPI;";
+        }
+
+        [HttpPost]
+
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from tbl_login where usarname='"+acc.Name+"' and password'"+acc.Password+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+            
+            
+        }
+    }
+}
